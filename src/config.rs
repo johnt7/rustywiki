164863efb26c325e00e2146b479c5c3c1631e862
jturnersf <jturner@starfishmedical.com>
2020-08-09 17:03:40 -0700

    comments

--- src/config.rs
+++ src/config.rs
@@ -4,16 +4,11 @@ use std::{
 };
 
 use super::wikifile;
-/*
-#[derive(Deserialize, Serialize, Debug)]
-pub struct ConfigContainer {
-    pub config : ConfigurationStruct,
-    pub header : wikifile::PageRevisionStruct
-}
-*/
+
 #[derive(Deserialize, Serialize, Debug, Default)]
 #[serde(rename_all = "PascalCase")]
 #[serde(default)]
+/// represents the tinywiki configuration file
 pub struct ConfigurationStruct {
 	pub case_sensitive : bool, // This should be set if the file system and thus wiki page names are case sensitive. If in doubt set to false.
 	pub authenticationequired_for_read : bool, // If true unautheticated users can read wiki pages
@@ -28,25 +23,15 @@ pub struct ConfigurationStruct {
 	pub admin_pages : Vec<String> // An array of pages and rest calls only available to admim users
 }
 
-/// Tries to load the config file for a tiny wiki
+/// Loads it into a rocket sharable, with a lock
 pub fn load_config() -> Result<wikifile::WikiStruct<ConfigurationStruct>, Box<dyn error::Error>> {
 	return Ok(wikifile::WikiStruct(RwLock::new( load_config_int()? )))
 }
 
+/// try to load core structure from the core data for a tiny wiki config
 pub fn load_config_int() -> Result<wikifile::WikiContainer<ConfigurationStruct>, Box<dyn error::Error>> {
     if let Ok((cfg, hdr)) = wikifile::load_parts("site/wiki/_config/current") {
         return Ok(wikifile::WikiContainer{data: serde_json::from_str(&cfg)?, header: hdr});
     }
     Err("Failed to load".into())
 }
-/*
-impl ConfigContainer {
-	pub fn auth_req_read(&self) -> bool {
-		self.config.authenticationequired_for_read
-	}
-	pub fn auth_req_log(&self) -> bool {
-		error!("auth for logging?");
-		self.config.authentication_required_for_logging
-	}
-}
-*/
