a8e1b18c5803e4bbfc63e1cf70b6566a1d762867
jturnersf <jturner@starfishmedical.com>
2020-08-10 18:11:33 -0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,16 @@
 #[macro_use] extern crate log;
 #[macro_use] extern crate serde_derive;
 
-// TODO - refactor clap and other parts of main
+// TODO - refactor main
 //      - test user auth
 //      - media index, generate, return and master reset
-//      - user api
+//      - user api calls
 //      - centralized file handling
 //      - cleanups
+//      - wikisave should put user into version info
+//      - fixes to index.html
+//          update revision data
+//      - look at other loggers
 
 
 use std::{
@@ -360,63 +364,14 @@ fn create_rocket(_cmd_cfg: cmdline::ConfigInfo) -> rocket::Rocket {
 
 
 fn main() {
-    let config = cmdline::get_command_line();
    /*
-    // TODO, look at alternative logger than simplelog
     CombinedLogger::init(
         vec![
-//            TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed),
+            TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed),
             WriteLogger::new(LevelFilter::Info, Config::default(), File::create("site/my_rust_binary.log").unwrap()),
         ]
     ).unwrap();
     */
+    let config = cmdline::get_command_line();
     create_rocket(config).launch();
 }
-
-
-
-
-// command line parsing.  TODO Not sure how to move to other module with macros
-/*
-/// Command line configuration information
-#[derive(Debug)]
-pub struct ConfigInfo {
-    pub log_file_name : String,
-    pub site : String,
-    pub console_log : bool,
-    pub cert : String,
-    pub key : String,
-    pub port : u16
-}
-
-/// Loads information from the command line
-pub fn get_command_line() -> ConfigInfo {
-    let matches = clap_app!(rtinywiki =>
-        (version: "0.0.0")
-        (author: "Johnt <johnt7@gmail.com>")
-        (about: "Rust Tiny Wiki")
-        (@arg logfile: -l --log +takes_value "Log file name")
-        (@arg cert: -c --cert +takes_value "Server certificate file")
-        (@arg site: -s --site +takes_value "Location of site directory")
-        (@arg key: -k --key +takes_value "Server key file")
-        (@arg port: -p --port +takes_value "Port number")
-        (@arg consoleLog: -d --dump "Dump log to console?")
-    ).get_matches();
-    ConfigInfo {
-        log_file_name : matches.value_of("logfile").unwrap_or("site/wikiserver.log").to_string(),
-        site : matches.value_of("site").unwrap_or("site").to_string(),
-        cert : matches.value_of("cert").unwrap_or("server.cert").to_string(),
-        key : matches.value_of("key").unwrap_or("server.key").to_string(),
-        console_log : !matches.is_present("consoleLog"),
-        port : matches.value_of("port").unwrap_or("9990").parse().unwrap_or(9990)
-    }
-}
-*/
-
-
-
-//
-// TODO list
-// index.html
-//    update revision date
-// wikisave - update user
